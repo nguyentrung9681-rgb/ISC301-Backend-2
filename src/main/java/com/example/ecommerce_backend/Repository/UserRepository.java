6e84be39c7daf877c35b03ed70e786da01f2c67c
nguyentrung9681-rgb <nguyentrung9681@gmail.com>
2026-06-18 15:30:18 +0700

    Comit Login, Logout, AuthO, profile

--- src/main/java/com/example/ecommerce_backend/Repository/UserRepository.java
+++ src/main/java/com/example/ecommerce_backend/Repository/UserRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     List<User> findByRole(Role role);
+    Optional<User> findByEmail(String email);
     @Query("""
             SELECT u FROM User u
             WHERE u.role = :role
