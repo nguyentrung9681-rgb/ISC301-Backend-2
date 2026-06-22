882cd7e93052991df61b1bfb31dd00004ad8ca40
nguyentrung9681-rgb <nguyentrung9681@gmail.com>
2026-06-22 22:58:34 +0700

    Revert "Update WebConfig"
    This reverts commit 098993eeacfb68a28252c7f6138d43ab37183550.

--- src/main/java/com/example/ecommerce_backend/Config/WebConfig.java
+++ src/main/java/com/example/ecommerce_backend/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce_backend.Config;
+package com.example.ecommerce_backend.config;
 
 import com.example.ecommerce_backend.Interceptor.RoleInterceptor; // Thêm import này
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Áp dụng cấu hình cho tất cả các URL API
                 .allowedOrigins("http://localhost:3000") // Chỉ cho phép nguồn từ ReactJS port 3000 gọi tới
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Cho phép các phương thức HTTP cơ
-                                                                                    // bản
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Cho phép các phương thức HTTP cơ bản
                 .allowedHeaders("*") // Cho phép tất cả các loại Headers mà Axios gửi lên
                 .allowCredentials(true) // Cho phép gửi kèm Cookies hoặc thông tin xác thực
                 .maxAge(3600); // Thời gian trình duyệt được phép lưu cache kết quả Preflight request
